1224025b1f8b97f54b5d43408f69a1ae74de731a
Mark Kubacki <mark@hurrikane.de>
2026-07-05 14:43:07 +0200

    Switch to 'zig build'

---
+++ build.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "check-bootc-and-reboot",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = .ReleaseSmall,
+            .single_threaded = true,
+            .unwind_tables = .none,
+        }),
+    });
+    exe.stack_size = 0; // Go with the OS’ default. Saves 2 syscalls to make it 8 MiB.
+    b.installArtifact(exe);
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_exe_tests.step);
+}
