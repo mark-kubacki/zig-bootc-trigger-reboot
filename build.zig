ba42907de98bbcf4a0fe33394492aeacb25c1d33
Mark Kubacki <mark@hurrikane.de>
2026-07-29 02:39:02 +0200

    build: Target RVA23 RISC-V

--- build.zig
+++ build.zig
@@ -3,7 +3,12 @@ const std = @import("std");
 const targets: []const std.Target.Query = &.{
     .{ .cpu_arch = .x86_64, .os_tag = .linux, .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v2 } },    // without AVX
     .{ .cpu_arch = .aarch64, .os_tag = .linux, .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 } },  // Radxa Zero, RPi 2W
-    .{ .cpu_arch = .riscv64, .os_tag = .linux },
+    .{
+        .cpu_arch = .riscv64,
+        .os_tag = .linux,
+        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.baseline_rv64 }, // Contains .d for lp64d.
+        .cpu_features_add = std.Target.riscv.featureSet(&[_]std.Target.riscv.Feature{ .rva23u64, .v }), // RVA23
+    },
 };
 
 pub fn build(b: *std.Build) !void {
